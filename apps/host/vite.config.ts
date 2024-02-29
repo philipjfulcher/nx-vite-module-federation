6e118cb11ccb7773de92741565444941bf9748a0
Philip Fulcher <philip@nrwl.io>
2024-02-29 14:46:18 -0700

    migrate to nx 17.3.2

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import federation from '@originjs/vite-plugin-federation'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/host',
   server: {
     port: 4200,
@@ -17,20 +18,22 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    nxViteTsPaths(),
+    // nxViteTsPaths(),
     federation({
       name: 'host',
       remotes: {
         remoteApp: 'http://localhost:4301/assets/remoteEntry.js',
       },
-      shared: ['react','react-dom']
-    })
+      shared: ['react', 'react-dom'],
+    }),
   ],
   build: {
+    outDir: '../../dist/apps/host',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     modulePreload: false,
     target: 'esnext',
     minify: false,
     cssCodeSplit: false,
-    outDir: '../../dist/apps/host'
-  }
-})
+  },
+});
--- apps/remote/vite.config.ts
+++ apps/remote/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/remote',
   server: {
     port: 4201,
@@ -19,19 +20,21 @@ export default defineConfig({
     react(),
     nxViteTsPaths(),
     federation({
-      name: "remote_app",
-      filename: "remoteEntry.js",
+      name: 'remote_app',
+      filename: 'remoteEntry.js',
       exposes: {
-        './Button': './apps/remote/src/components/Button'
+        './Button': './apps/remote/src/components/Button',
       },
-      shared: ['react','react-dom']
-    })
+      shared: ['react', 'react-dom'],
+    }),
   ],
   build: {
+    outDir: '../../dist/apps/remote',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     modulePreload: false,
     target: 'esnext',
     minify: false,
-    cssCodeSplit: false
-  }
-})
-
+    cssCodeSplit: false,
+  },
+});
